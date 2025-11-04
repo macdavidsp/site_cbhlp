735e23b7d1addb086fdda5e8dc5a133b6f379798
macdavidsp <macdavid@uft.edu.br>
2025-11-04 18:05:40 -0300

    nova chave api

--- base_script.R
+++ base_script.R
@@ -5,8 +5,10 @@
 # 
 usethis::browse_github()
 
-usethis::
-  
-# https://wdxwgtkgkhdeslmbkmnt.supabase.co
+# https://cbhlp.supabase.co
 
 # eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndkeHdndGtna2hkZXNsbWJrbW50Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE4Nzk5NzgsImV4cCI6MjA3NzQ1NTk3OH0._UfUZYLU9tY-nmWYt88fO9sOifCugPxPfQ6rQ-d0t_M
+  
+pacman::p_load(httr, jsonlite)
+
+# https://script.google.com/a/macros/mail.uft.edu.br/s/AKfycbwxsGyjekkgQx-OZbSrAufOYiSCPCWBFmlBS2GhnzP78I2EQCkJu9LPmbl3cWVO64jBSw/exec
